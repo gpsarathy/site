10604aee7c3975910f93090e8b019398ff9b0354
Parthasarathy <{ID}+{username}@users.noreply.github.com>
2024-01-29 12:03:08 +0530

    added skills section

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://gpsarathy.github.io',
+  // base: '/site',
   integrations: [mdx(), sitemap(), react(),
   tailwind(
     {
---
+++ src/pages/about/MySkills.tsx
@@ -0,0 +1,50 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
+
+export default function(){
+    return <>
+    <Accordion type="multiple">
+    <AccordionItem value="item-1" >
+      <AccordionTrigger>Programming Languages</AccordionTrigger>
+      <AccordionContent>
+      Java, Javascript, Typescript, Python
+      </AccordionContent>
+    </AccordionItem>
+    <AccordionItem value="item-2">
+      <AccordionTrigger>Scripting Languages</AccordionTrigger>
+      <AccordionContent>
+      Terraform, HTML, CSS, Shell, JSP
+      </AccordionContent>
+    </AccordionItem>
+    <AccordionItem value="item-3">
+      <AccordionTrigger>Frameworks and UI</AccordionTrigger>
+      <AccordionContent>
+      Spring Boot, NodeJS, ReactJS Angular 6+, Astro ( This site built on <b className=" text-lg">🚀</b> )
+      </AccordionContent>
+    </AccordionItem>
+    <AccordionItem value="item-4">
+      <AccordionTrigger>Persistence</AccordionTrigger>
+      <AccordionContent>
+      SQL, No-SQL, MySQL, MongoDB, Hibernate, Firestore, Firebase
+      </AccordionContent>
+    </AccordionItem>
+    <AccordionItem value="item-5">
+      <AccordionTrigger>Cloud & CI/CD</AccordionTrigger>
+      <AccordionContent>
+      Google Cloud Platform, AWS, Harness, Open-Shift Tekton
+      </AccordionContent>
+    </AccordionItem>
+    <AccordionItem value="item-6">
+      <AccordionTrigger>Others</AccordionTrigger>
+      <AccordionContent>
+      Basics of Flutter, Dart, Unreal Engine, C++ and Etc
+      </AccordionContent>
+    </AccordionItem>
+  </Accordion>
+
+  </>
+}
